51e4c7fbc2fbc438d649915ad4d7b0195716761b
Laiba-Malik0 <laibamlk834@gmail.com>
2024-12-27 19:37:18 +0500

    first commit

---
+++ work/main.js
@@ -0,0 +1,74 @@
+// alerts:
+
+///////task1///////
+
+alert("Welcome to Javascript!");
+
+///////task2///////
+
+var Name = prompt("What is your name?");
+alert("Hello!" + " " + Name);
+
+// variables for String:
+
+///////task1/////////
+
+var a = prompt("First name");
+var b = prompt("Last name");
+var c = a + b;
+alert(c);
+
+///////task2////
+
+var A = prompt("Your favorite hobby? ");
+alert("Your Favorite hobby is" + " " + A);
+
+// variables for numbers
+
+//////task1/////
+
+var a = prompt("Current_year");
+var b = prompt(" Your Birth_year");
+var c = a - b;
+alert("Your age is" + " " + c);
+
+/////task2/////
+
+var length = prompt("lenth of rectangle");
+var width = prompt("width of rectangle");
+var c = length * width;
+alert("Area of Rectangle is" +" " + c);
+
+/////variables name legal and illegal////
+
+/////task1/////
+
+var a = prompt("what are three legal variables name in java?");
+alert(a);
+var b = prompt("what are three illegal variables name in java?")
+alert(b);
+/////task2////
+
+var a = prompt("Your age?");
+var b = prompt("Your friend's Age?");
+alert("Your Age is" + " " + a + "and Your friend's Age is" + " " + b);
+
+///////Math Expressions:Familiar Operators:
+
+var a = prompt("enter first num for sum");
+var b = prompt("enter second num");
+var c = a + b;
+alert("The sum is :" + " " + c);
+var a = prompt("enter first num for product");
+var b = prompt("enter second num for product");
+var c = a * b;
+alert("The product is :" + " " + c);
+var a = prompt("enter first num for differnce");
+var b = prompt("enter second num for differnce");
+var c = a - b;
+alert("The remainder is :" + " " + c);
+var a = prompt("enter first num for remainder");
+var b = prompt("enter second num for remainder");
+var c = a % b;
+alert("The remainder is:" + " " + c);
+
